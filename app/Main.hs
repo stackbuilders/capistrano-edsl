ec2830da26f9ee7061b181e382cbe96549a80548
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2021-09-08 00:41:07 -0500

    Rename Lib to Capistrano.EDSL

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Lib
+import Capistrano.EDSL
 
 main :: IO ()
 main = runDeployStarting
--- src/Capistrano/EDSL.hs
+++ src/Capistrano/EDSL.hs
@@ -1,4 +1,4 @@
-module Lib where
+module Capistrano.EDSL where
 
 import Control.Exception
 import Control.Monad.Writer
